73d1cc70a26302eb06f2d85c0db54f2c3fb21617
sathya <satyavardhanreddy12@gmail.com>
2023-04-30 09:40:07 +0530

    first commit

---
+++ total invoice amount.py
@@ -0,0 +1,11 @@
+python_p = 499
+python_l_price = 855
+ds_price = 645
+gst = 12/100
+delivery_charges = 250
+book1 = int(input("Enter number of python programming books yoy want : "))
+book2 = int(input("Enter number of python libraries cookbooks  yoy want : "))
+book3 = int(input("Enter number of data science in  python  books yoy want : "))
+total_price = book1*python_p + book2*python_l_price + book3*ds_price
+total_invoice_amount =total_price + total_price * gst + delivery_charges
+print("Total invoice amount = ",total_invoice_amount)
